110225eed6a8c1aaf6fc448cbc5f41f1c71cef18
Popov A.S <antoxamad@gmail.com>
2022-05-14 23:46:30 +0300

    Работа с элементами на странцие. Операторы getElementById, getElementsByTagName, getElementsBtClassName. Новые операторы для работы с элементами querySelectorAll, querySelector.

---
+++ elements/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const box = document.getElementById("box");
+console.log(box);
+
+const btns = document.getElementsByTagName("button");
+console.log(btns);
+console.log(btns[2]);
+
+const circles = document.getElementsByClassName("circle");
+console.log(circles);
+
+const hearts = document.querySelectorAll(".heart");
+hearts.forEach((item) => console.log(item));
+
+const oneHeart = document.querySelector(".heart");
+console.log(oneHeart);
